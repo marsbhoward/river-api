09545087042219319e708c5097539635176fd5d2
Mars <marsbhoward@gmail.com>
2020-04-12 15:07:45 -0400

    users index and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def index
+  	users = User.all
+  	render json: users
+  end
+
+  def create()
+    user = User.create(username: user_params[:username],email: user_params[:email])
+    render json: player
+  end 
+  
+  private
+  	def user_params
+    	params.require(:user).permit(:id, :username, :email)
+  	end   	
 end
