39c4f7a8a3a74aec479fe9e6622f3801efec7608
Mars <marsbhoward@gmail.com>
2020-04-13 13:43:09 -0400

    add user streams index

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -1,3 +1,11 @@
 class UserStreamsController < ApplicationController
+  def index
+  	user_streams = UserStream.all
+  	render json: user_streams
+  end
+
+  def userstream_params
+    params.permit(:user_id,:stream_id)
+  end  
 
 end
