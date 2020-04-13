e6e4e9e1ec9590e72adf3157fe957577fb682c2e
Mars <marsbhoward@gmail.com>
2020-04-13 13:13:49 -0400

    user streams controller

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -1,2 +1,27 @@
 class UserStreamsController < ApplicationController
+  def index
+  	user_streams = UserStream.all
+  	render json: user_streams
+  end
+
+  def create
+  	user_stream = UserStream.find_or_create_by(user_id: user_params[:user_id],stream_id: user_params[:stream_id])
+  	render json: user_stream
+  end
+
+  def show
+    user_stream = UserStream.find(stream_params[:id])
+    render json: user_stream
+  end
+
+  def destroy
+  	user_stream = UserStream.find(user_id: user_params[:user_id],stream_id: user_params[:stream_id])
+  	user_stream.destroy
+  	render :json: user_stream
+  end
+
+  def userstream_params
+    params.permit(:user_id,:stream_id)
+  end  
+
 end
