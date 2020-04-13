b1f7f9ef90c5887a213a4ee22fe00e50084d3fc0
Mars <marsbhoward@gmail.com>
2020-04-13 13:23:23 -0400

    remove user streams methods

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -1,22 +1,3 @@
 class UserStreamsController < ApplicationController
-  def index
-  	user_streams = UserStream.all
-  	render json: user_streams
-  end
-
-  def create
-  	user_stream = UserStream.find_or_create_by(user_id: userstream_params[:user_id],stream_id: userstream_params[:stream_id])
-  	render json: user_stream
-  end
-
-  def destroy
-  	user_stream = UserStream.find(user_id: userstream_params[:user_id],stream_id: userstream_params[:stream_id])
-  	user_stream.destroy
-  	render :json: user_stream
-  end
-
-  def userstream_params
-    params.permit(:user_id,:stream_id)
-  end  
 
 end
