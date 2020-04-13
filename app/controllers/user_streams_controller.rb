70ab439cb93759161e5f61e5323aaef122294bbf
Mars <marsbhoward@gmail.com>
2020-04-13 13:21:23 -0400

    user streams params fix

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -5,12 +5,12 @@ class UserStreamsController < ApplicationController
   end
 
   def create
-  	user_stream = UserStream.find_or_create_by(user_id: user_params[:user_id],stream_id: user_params[:stream_id])
+  	user_stream = UserStream.find_or_create_by(user_id: userstream_params[:user_id],stream_id: userstream_params[:stream_id])
   	render json: user_stream
   end
 
   def destroy
-  	user_stream = UserStream.find(user_id: user_params[:user_id],stream_id: user_params[:stream_id])
+  	user_stream = UserStream.find(user_id: userstream_params[:user_id],stream_id: userstream_params[:stream_id])
   	user_stream.destroy
   	render :json: user_stream
   end
