84dd06a1152dfa9db290a58cd04b1f176812ea0d
Mars <marsbhoward@gmail.com>
2020-04-15 21:17:24 -0400

    add user_stream params to index

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -1,6 +1,6 @@
 class UserStreamsController < ApplicationController
   def index
-  	user_streams = UserStream.where(user_id: user_stream_params[:user_id])
+  	user_streams = UserStream.where(user_id: user_stream_params[:user_id],stream_id: user_stream_params[:stream_id])
   	render json: user_streams
   end
 
