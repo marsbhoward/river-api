124161bc94077a977234256234eb473d4c375a29
Mars <marsbhoward@gmail.com>
2020-05-12 11:03:08 -0400

    fix user stream up date

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -16,7 +16,7 @@ class UserStreamsController < ApplicationController
 
   def update
     user_stream = UserStream.where(user_id: user_stream_params[:user_id],id: user_stream_params[:id])
-    user_stream.update(:selected => user_stream_params[:selected])
+    UserStream.update(user_stream.id, :selected => user_stream_params[:selected])
     render json: user_stream
   end
 
