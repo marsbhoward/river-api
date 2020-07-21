5e936b8ae3b8fdb9e7e0905de0e3a1619bdf4a64
Mars <marsbhoward@gmail.com>
2020-07-20 22:16:44 -0400

    add update to movies_controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,10 +3,16 @@ class MoviesController < ApplicationController
 	def index
 		movies = Scraper.new.get_movies(Stream.find(params[:stream_id]))
   		render json: movies
-	end	
+	end
+
+  def update
+    movie = Movie.find(params[:movie_id]))
+    movie.update(:youtube_id => movies_params[:youtube_id])
+    render json: movie
+  end	
 
   	def movies_params
-    	params.permit(:movie_id)
+    	params.permit(:movie_id,:title,:year,:youtube_id)
   	end
 
    	def stream_params
