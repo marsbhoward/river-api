7e8dea08f19436e23fd7caa7baa524b8b7d6b51d
Mars <marsbhoward@gmail.com>
2020-09-01 11:30:55 -0400

    add id conditon db

--- db/migrate/20200411203650_create_user_streams.rb
+++ db/migrate/20200411203650_create_user_streams.rb
@@ -1,6 +1,6 @@
 class CreateUserStreams < ActiveRecord::Migration[6.0]
   def change
-    create_table :user_streams do |t|
+    create_table :user_streams, id: true do |t|
       t.references :user, null: false, foreign_key: true
       t.references :stream, null: false, foreign_key: true
       t.boolean :selected, null:false, :default => "true"
