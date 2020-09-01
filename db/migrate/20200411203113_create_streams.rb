bac2601292a934eafb2614f462d3e479cbb8a1c1
Mars <marsbhoward@gmail.com>
2020-09-01 11:38:44 -0400

    remove id condition

--- db/migrate/20200411203113_create_streams.rb
+++ db/migrate/20200411203113_create_streams.rb
@@ -1,6 +1,6 @@
 class CreateStreams < ActiveRecord::Migration[6.0]
   def change
-    create_table :streams, id: true do |t|
+    create_table :streams do |t|
       t.string :name
       t.string :url
 
