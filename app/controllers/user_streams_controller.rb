87fc187aada180eed98238058798e39f73cd4312
Mars <marsbhoward@gmail.com>
2020-10-07 19:09:53 -0400

    sort user_streams json by id

--- app/controllers/user_streams_controller.rb
+++ app/controllers/user_streams_controller.rb
@@ -1,6 +1,6 @@
 class UserStreamsController < ApplicationController
   def index
-  	user_streams = UserStream.where(user_id: user_stream_params[:user_id])
+  	user_streams = UserStream.where(user_id: user_stream_params[:user_id]).sort()
   	render json: user_streams
   end
 
