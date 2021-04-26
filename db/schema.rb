f7260cfc0cb9ca781d0caeed529cea5e7a390909
Mars <marsbhoward@gmail.com>
2021-04-26 14:46:05 -0400

    add posters to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_145319) do
+ActiveRecord::Schema.define(version: 2021_04_26_184405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_145319) do
     t.bigint "stream_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "poster"
     t.index ["slug", "year"], name: "index_movies_on_slug_and_year", unique: true
     t.index ["stream_id"], name: "index_movies_on_stream_id"
   end
